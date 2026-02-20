bb35f54a074638f7a1c20c6a0115c22b68a599e1
Whoisbezo.fx <bezzo19@gmx.de>
2026-02-20 22:49:32 +0100

    app.py aktualisieren

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
+import os, requests, sqlite3
 import discord
-
 from discord.ext import commands
-
-Token = 
-
-bot = discord.Bot()
+from dotenv import load_dotenv
 
 
-bot.run(Token)
+load_dotenv ()
+# ------------- Settings -------------
+
+TOKEN = os. getenv("'DISCORD_TOKEN")
+GUILD_ID = int (os. getenv("GUILD_IDS"))
+
+
+intents = discord. Intents.default()
+intents. message_content = True
+bot = commands. Bot (command_prefix="!", intents=intents)
+
+# ------------- Bot Start -------------
+
+@bot.event
+async def on_ready () :
+    print (f"🤖 | {bot. user} ist online.")
+    await bot. sync_commands ()
+    print (" 🧩 | Commands synchronisiert.")
+
+# # ------------- Tests -------------
+@bot. slash_command(guild_ids=["GUILD_ID"])
+async def hello(ctx):
+    await ctx. respond ("Hello!")
+    
+bot. run(TOKEN)
